97ae127ddba24bfbdba8347148cb8296b542e8d2
Andy Weidenbaum <archbaum@gmail.com>
2016-12-03 13:16:10 -0800

    checkout commit ID in panda

--- Build.pm
+++ Build.pm
@@ -8,6 +8,8 @@ class Build is Panda::Builder
     method build($workdir)
     {
         run qw<git clone https://github.com/atweiden/finn-examples t/data>;
+        chdir 't/data';
+        run qw<git checkout 70d39d18a694c17657f8a9923a84d68d50e45df1>;
     }
 }
 
